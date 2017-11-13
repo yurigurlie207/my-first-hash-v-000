94c65d54fa93d5f3767d07d7fabd828902fe94b9
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-13 18:13:06 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,7 +9,7 @@ def shipping_manifest
   #fill that hash with key/value pairs that describe the following information:
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   shipping_manifest = {"whale bone corsets" => 5, "oil paintings" => 3, "porcelain vases" => 2}
-  return shipping_manifest["oil paintings"]
+
 
 end
 
@@ -17,7 +17,7 @@ def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
   #your code here!
-
+  return shipping_manifest["oil paintings"]
 end
 
 def adding
